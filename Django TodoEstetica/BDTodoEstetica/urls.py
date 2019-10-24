3ac0536f58d8a1f1e56d1b035d8e106aa03065eb
nicolasignacio97 <53886403+nicolasignacio97@users.noreply.github.com>
2019-10-24 19:25:49 -0300

    django 1.0.2

--- Django TodoEstetica/BDTodoEstetica/urls.py
+++ Django TodoEstetica/BDTodoEstetica/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.inicio, name = 'inicio'),
+    path('', views.inicio, name = 'inicio'),
     path('tienda/', views.tienda, name = 'tienda'),
 
 ]
